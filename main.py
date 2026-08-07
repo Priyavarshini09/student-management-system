d5ba3d7246b9bb6246a6ae79911dc40330e3bc27
Priyavarshini09 <priya5varshini2006@gmail.com>
2026-08-07 21:25:28 +0530

    Add basic menu

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+print("===================================")
+print("   Student Management System")
+print("===================================")
 
+while True:
+    print("\nMenu")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        name = input("Enter student name: ")
+        print(name, "added successfully!")
+
+    elif choice == "2":
+        print("No students available.")
+
+    elif choice == "3":
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
