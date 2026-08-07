a55e44ddb4488ee6e26e50e3c58d350cd696d44e
Priyavarshini09 <priya5varshini2006@gmail.com>
2026-08-07 21:28:03 +0530

    Store students in a list

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+students = []
+
 print("===================================")
 print("   Student Management System")
 print("===================================")
@@ -12,10 +14,16 @@ while True:
 
     if choice == "1":
         name = input("Enter student name: ")
-        print(name, "added successfully!")
+        students.append(name)
+        print("Student added successfully!")
 
     elif choice == "2":
-        print("No students available.")
+        if len(students) == 0:
+            print("No students found.")
+        else:
+            print("\nStudent List")
+            for i, student in enumerate(students, start=1):
+                print(i, student)
 
     elif choice == "3":
         print("Thank you!")
